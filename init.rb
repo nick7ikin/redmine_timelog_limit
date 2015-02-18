d672f51a32e70631ea1e465483187575dc8f8d0e
Nick Semikin <n.semikin@mobidev.biz>
2015-02-18 07:12:18 +0200

    cleaned commented code. updated plugin description

--- init.rb
+++ init.rb
@@ -3,18 +3,12 @@ require 'redmine'
 Rails.configuration.to_prepare do
   require 'time_entry'
   TimeEntry.send(:include, TimeEntryPatch)
-  # require_dependency 'timelog_controller'
-  # TimelogController.send(:include, TimeEntryPatch)
 end
-
-
-
 
 Redmine::Plugin.register :redmine_timelog_limit do
   name 'Redmine Timelog Limit plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Nikolay Semikin'
+  description 'You can limit dates to which user is allowed to log his time'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/nick7ikin/redmine_timelog_limit'
 end
