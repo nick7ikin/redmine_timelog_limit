7fb8e478262efa27ec7be18d2a5e2d06e3c3197a
Nick Semikin <n.semikin@mobidev.biz>
2015-02-27 13:06:01 +0200

    added manual date check before save timelog; added exclusive/exceptional users list

--- lib/time_entry_patch.rb
+++ lib/time_entry_patch.rb
@@ -10,10 +10,35 @@ module TimeEntryPatch
 
       protected
       def check_limit
+        isExclusiveUser = TimelogLimit.exceptional_user_ids.include?(User.current.id)
+        if isExclusiveUser
+          return
+        end
+
+        check_day_offset_limit
+        check_date_limit
+      end
+
+      def check_date_limit
+        limit_date = TimelogLimit.timelog_manual_global_date_limit_date
+        if limit_date.nil?
+          return
+        end
+        time_entry_date = spent_on
+        days_since_date_limit = time_entry_date - limit_date
+        if days_since_date_limit < 0
+          # errors.add :spent_on, "you can't log before #{limit_date}"
+          error_str = l(:must_be_after) + TimelogLimit.timelog_manual_global_date_limit_date
+          errors.add :spent_on, l(error_str)
+        end
+      end
+
+      def check_day_offset_limit
         time_entry_date = spent_on
         num_of_days = Date.today - time_entry_date
         timelog_limit = TimelogLimit.timelog_days_auto_offset_limit
         if num_of_days > timelog_limit && !User.current.admin?
+          error_str = l(:must_be_closer_to_the_current_day) + timelog_limit.to_s
           errors.add :spent_on, "you can't log later then #{timelog_limit} days"
         end
       end
--- lib/timelog_limit.rb
+++ lib/timelog_limit.rb
@@ -4,9 +4,53 @@ module TimelogLimit
   DEFAULT_DAYS_LIMIT = 31   #  it's ~1 month
 
   class << self
+
     def timelog_days_auto_offset_limit
       by_settigns = Setting.plugin_redmine_timelog_limit['timelog_days_auto_offset_limit'].to_i
       by_settigns > 0 ? by_settigns : DEFAULT_DAYS_LIMIT
+    end
+
+    def timelog_manual_global_date_limit_string
+      Setting.plugin_redmine_timelog_limit['timelog_manual_global_date_limit'].to_s
+    end
+
+    def timelog_manual_global_date_limit_date
+      Setting.plugin_redmine_timelog_limit['timelog_manual_global_date_limit'].to_s.to_date
+    end
+
+    def active_users_without_exceptional
+      exceptional_ids = exceptional_user_ids
+      without_exceptional = User.active.sorted.reject { |user| exceptional_ids.include?(user.id) }
+      users_to_select_field_options(without_exceptional)
+    end
+
+    def exceptional_users_hash
+      exceptional_ids = User.find(exceptional_user_ids)
+      users_to_select_field_options(exceptional_ids)
+    end
+
+    def exceptional_user_ids
+      user_ids = []
+      exceptional_users_id_string.split(",").each { |user_id| user_ids.push(Integer(user_id))}
+      user_ids
+    end
+
+    def exceptional_users_id_string
+      Setting.plugin_redmine_timelog_limit['exclusive_users'].to_s
+    end
+
+
+    protected
+    def users_to_select_field_options(users)
+      users_hash = {}
+      users.each { |user|
+        users_hash[user.name] = user.id
+      }
+      users_hash
+    end
+
+    def exceptional_users
+      User.find(exceptional_user_ids)
     end
 
   end
